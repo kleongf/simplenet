aaa505ccc94f110a45fd2db67ada44b2578ffefd
Kevin Fong <kf53307@pausd.us>
2026-07-14 13:38:45 -0700

    conv2d module very very hard still dont completely understand

--- src/simplenet/modules.py
+++ src/simplenet/modules.py
@@ -82,6 +82,76 @@ class BatchNorm1D(Module):
     
     def parameters(self):
         return [self.gamma, self.beta]
+    
+class Conv2d(Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
+        # same initialization as pytorch from kaiming uniform or somethin lol
+        self.weight = Tensor(np.random.randn(out_channels, in_channels, kernel_size, kernel_size) * np.sqrt(1. / (in_channels * kernel_size * kernel_size)))
+        self.bias = Tensor(np.random.randn(out_channels) * np.sqrt(1. / (in_channels * kernel_size * kernel_size)))
+        self.stride, self.padding = stride, padding
+
+    def _out_hw(self, height, width):
+        kernel_height, kernel_width = self.weight.data.shape[2], self.weight.data.shape[3]
+        # output kernel dimensions, not using dilation for now (set to 1)
+        out_height = (height + 2 * self.padding - kernel_height) // self.stride + 1
+        out_width = (width + 2 * self.padding - kernel_width) // self.stride + 1
+        return out_height, out_width
+
+    def im2col(self, x):
+        # x: (batch, channels, height, width)
+        batch_size, channels, height, width = x.data.shape
+        kernel_height, kernel_width = self.weight.data.shape[2], self.weight.data.shape[3]
+        out_height, out_width = self._out_hw(height, width)
+
+        # pad the input
+        x_padded = np.pad(x.data, ((0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding)), mode='constant')
+
+        # im2col
+        cols_data = np.zeros((batch_size, channels * kernel_height * kernel_width, out_height * out_width))
+        for oy in range(out_height):
+            for ox in range(out_width):
+                patch = x_padded[:, :, oy*self.stride:oy*self.stride+kernel_height, ox*self.stride:ox*self.stride+kernel_width]
+                cols_data[:, :, oy*out_width + ox] = patch.reshape(batch_size, -1)
+
+        cols = Tensor(cols_data, (x,))
+        def _backward():
+            # col2im is exactly the adjoint (scatter-add) of im2col's gather,
+            # so it's reused here as im2col's backward.
+            x.grad += self.col2im(Tensor(cols.grad), x.data.shape).data
+        cols._backward_fn = _backward
+        return cols
+
+    def col2im(self, cols, x_shape):
+        # cols: (batch, channels * kernel_height * kernel_width, out_height * out_width)
+        batch_size, channels, height, width = x_shape
+        kernel_height, kernel_width = self.weight.data.shape[2], self.weight.data.shape[3]
+        out_height, out_width = self._out_hw(height, width)
+
+        x_padded = np.zeros((batch_size, channels, height + 2 * self.padding, width + 2 * self.padding))
+        for oy in range(out_height):
+            for ox in range(out_width):
+                patch = cols.data[:, :, oy*out_width + ox].reshape(batch_size, channels, kernel_height, kernel_width)
+                x_padded[:, :, oy*self.stride:oy*self.stride+kernel_height, ox*self.stride:ox*self.stride+kernel_width] += patch
+        if self.padding > 0:
+            return Tensor(x_padded[:, :, self.padding:-self.padding, self.padding:-self.padding])
+        return Tensor(x_padded)
+
+    def forward(self, x):
+        # x: (batch, channels, height, width)
+        cols = self.im2col(x)
+        # reshape weight to (out_channels, in_channels * kernel_size * kernel_size)
+        weight_reshaped = self.weight.reshape((self.weight.data.shape[0], -1))
+        # (out_channels, 1) so it broadcasts against the channel axis of
+        # (batch, out_channels, out_height * out_width), not the spatial axis
+        bias_reshaped = self.bias.reshape((self.bias.data.shape[0], 1))
+        out = weight_reshaped @ cols + bias_reshaped
+        # reshape back to (batch, out_channels, out_height, out_width)
+        batch_size, _, height, width = x.data.shape
+        out_height, out_width = self._out_hw(height, width)
+        return out.reshape((batch_size, -1, out_height, out_width))
+
+    def parameters(self):
+        return [self.weight, self.bias]
 
 # activation function modules to make it easier to build a neural network
 class ReLU(Module):
